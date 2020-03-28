20d6d73059a9291e18a97301bd4fb4812aaa8396
Ari Davis <aridavis6688@gmail.com>
2020-03-28 07:13:53 +0700

    Change File Method

--- mouselogger.py
+++ mouselogger.py
@@ -1,13 +1,9 @@
 import pyxhook
 import os
 
-#get("mouse_log_file") -> Jikalau ada environment variables
-mouse_log_file = os.environ.get("mouse_log_file", os.path.expanduser("~/Desktop/MouseLogger.log"))
-
-
 def onMouseEvent(event):
     #membuka file mouse_log_file dengan mode a (append)
-    with open(mouse_log_file, 'a') as f :
+    with open('mouselogger.log', 'a') as f :
         f.write(str(event.WindowName))
         f.write(str(event.Position))
         f.write(str(event.MessageName))
